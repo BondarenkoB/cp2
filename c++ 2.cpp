7bbc4467151fd0fac67f18877ad53b47e9c5bcad
Bohdan <bohdanstep23@gmail.com>
2023-05-20 14:32:31 +0300

    done

--- c++ 2.cpp
+++ c++ 2.cpp
@@ -1,20 +1,38 @@
-﻿// c++ 2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <Windows.h>
 
-#include <iostream>
+using namespace std;
 
-int main()
+double calculateVolume(double length, double width, double height) 
 {
-    std::cout << "Hello World!\n";
+    return length * width * height;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void calculateParallelepipedVolume() 
+{
+    double length, width, height;
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    cout << "Обчислення об'єму паралелепіпеда." << endl;
+    cout << "Введіть вихідні дані:" << endl;
+
+    cout << "Довжина (см) -> ";
+    cin >> length;
+
+    cout << "Ширина (см) -> ";
+    cin >> width;
+
+    cout << "Висота (см) -> ";
+    cin >> height;
+
+    double volume = calculateVolume(length, width, height);
+
+    cout << "Об'єм: " << volume << " куб. см." << endl;
+}
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    calculateParallelepipedVolume();
+    return 0;
+}
